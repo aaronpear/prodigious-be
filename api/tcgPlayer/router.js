68b36397ab44187a95ab8f47d1c173d908728b1c
Aaron Reyes <56135909+aaronpear@users.noreply.github.com>
2022-08-25 12:00:55 -0700

    default endpoint now takes limit and offset params for pagination

--- api/tcgPlayer/router.js
+++ api/tcgPlayer/router.js
@@ -1,24 +1,17 @@
 const router = require('express').Router();
 const { axiosWithTCGPKey } = require('../utils/axiosWithTCGPKey');
 
-router.get('/', (req, res, next) => {
+// limit and offset parameters are used for pagination;
+// limit determines the amount of cards retrieved at a time and offset
+// determines the section of cards to retrieve (page 1 starts at offset = 0)
+router.get('/:limit/:offset', (req, res, next) => {
+    console.log(req.params);
     axiosWithTCGPKey()
-        .get('/catalog/products?categoryId=63&getExtendedFields=true&productTypes=Cards&limit=50')
+        .get(`/catalog/products?categoryId=63&getExtendedFields=true&productTypes=Cards&limit=${req.params.limit}&offset=${req.params.limit * req.params.offset}`)
         .then((response) => {
             res.status(200).json(response.data);
         })
         .catch(next);
 });
-
-router.get('/products', (req, res, next) => {
-    axiosWithTCGPKey()
-        .get('/catalog/products/229758?getExtendedFields=true')
-        .then((response) => {
-            console.log(response);
-            res.status(200).json(response.data.results);
-        })
-        .catch(next);
-});
-
 
 module.exports = router;
